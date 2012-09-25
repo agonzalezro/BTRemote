cf346567ea6944e32ab5fb8e42bf954f403878b1
Álex González <agonzalezro@gmail.com>
2012-09-25 23:34:58 +0200

    Before change the bt setup

--- gen/com/example/leftright/R.java
+++ gen/com/example/leftright/R.java
@@ -17,13 +17,19 @@ public final class R {
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
         public static final int connection=0x7f070001;
-        public static final int max_direction=0x7f070006;
-        public static final int max_speed=0x7f070004;
-        public static final int menu_settings=0x7f070008;
-        public static final int min_direction=0x7f070005;
-        public static final int min_speed=0x7f070003;
-        public static final int position=0x7f070007;
+        public static final int max_direction=0x7f07000a;
+        public static final int max_speed=0x7f070005;
+        public static final int menu_settings=0x7f07000e;
+        public static final int min_direction=0x7f070009;
+        public static final int min_speed=0x7f070004;
+        public static final int position=0x7f07000d;
+        public static final int textView1=0x7f070003;
+        public static final int textView2=0x7f070008;
         public static final int trimmers=0x7f070002;
+        public static final int txt_view_max_direction=0x7f07000c;
+        public static final int txt_view_max_speed=0x7f070007;
+        public static final int txt_view_min_direction=0x7f07000b;
+        public static final int txt_view_min_speed=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,9 +40,11 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int connection=0x7f040005;
+        public static final int direction=0x7f040008;
         public static final int hello_world=0x7f040001;
         public static final int loading=0x7f040004;
         public static final int menu_settings=0x7f040002;
+        public static final int speed=0x7f040007;
         public static final int title_activity_main=0x7f040003;
         public static final int trimmers=0x7f040006;
     }
--- src/com/example/leftright/MainActivity.java
+++ src/com/example/leftright/MainActivity.java
@@ -16,21 +16,25 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 
 public class MainActivity extends Activity implements SensorEventListener {
-	public Boolean ready = false;
+	public Boolean sensor_ready = false;
+	public Boolean bt_ready = false;
     private float z = 0, y = 0;
     
-    private Integer min_servo_direction = 60;
+    private Integer min_servo_direction = 65;
     private Integer max_servo_direction = 130;
-    private Integer min_servo_speed = 103;
+    private Integer min_servo_speed = 50;
     private Integer max_servo_speed = 127;
     
     private Integer previous_speed = 0;
@@ -44,7 +48,10 @@ public class MainActivity extends Activity implements SensorEventListener {
 	private SeekBar min_direction_seekbar = null;
 	private SeekBar max_direction_seekbar = null;
 	
-	private ToggleButton bt_connection = null;
+	private TextView txt_view_min_speed = null;
+    private TextView txt_view_max_speed = null;
+    private TextView txt_view_min_direction = null;
+    private TextView txt_view_max_direction = null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,18 +71,13 @@ public class MainActivity extends Activity implements SensorEventListener {
         setupAcelerometer();
         setupBluetooth();
     }
-    
-	/*@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if (connection != null) {
-			try {
-				connection.close();
-			} catch (IOException e) { }
-		}
-	}*/
 
     private void setupTrimmers() {
+        txt_view_min_speed = (TextView) findViewById(R.id.txt_view_min_speed);
+        txt_view_max_speed = (TextView) findViewById(R.id.txt_view_max_speed);
+        txt_view_min_direction = (TextView) findViewById(R.id.txt_view_min_direction);
+        txt_view_max_direction = (TextView) findViewById(R.id.txt_view_max_direction);
+    	
     	min_speed_seekbar = (SeekBar) findViewById(R.id.min_speed);
         max_speed_seekbar = (SeekBar) findViewById(R.id.max_speed);
         min_direction_seekbar = (SeekBar) findViewById(R.id.min_direction);
@@ -87,14 +89,18 @@ public class MainActivity extends Activity implements SensorEventListener {
         max_direction_seekbar.setEnabled(false);
         
         min_speed_seekbar.setProgress(min_servo_speed);
+        txt_view_min_speed.setText(String.valueOf(min_servo_speed));
         max_speed_seekbar.setProgress(max_servo_speed);
+        txt_view_max_speed.setText(String.valueOf(max_servo_speed));
         min_direction_seekbar.setProgress(min_servo_direction);
+        txt_view_min_direction.setText(String.valueOf(min_servo_direction));
         max_direction_seekbar.setProgress(max_servo_direction);
+        txt_view_max_direction.setText(String.valueOf(max_servo_direction));
         
         min_speed_seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
         	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         		min_servo_speed = min_speed_seekbar.getProgress();
-        	    Log.d("BT", String.valueOf(min_servo_speed));
+        		txt_view_min_speed.setText(String.valueOf(min_servo_speed));
         	}
 			public void onStartTrackingTouch(SeekBar seekBar) { }
 			public void onStopTrackingTouch(SeekBar seekBar) { }
@@ -103,7 +109,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         max_speed_seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
         	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         		max_servo_speed = max_speed_seekbar.getProgress();
-                Log.d("BT", String.valueOf(max_servo_speed));
+        		txt_view_max_speed.setText(String.valueOf(max_servo_speed));
         	}
 			public void onStartTrackingTouch(SeekBar seekBar) { }
 			public void onStopTrackingTouch(SeekBar seekBar) { }
@@ -112,7 +118,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         min_direction_seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
         	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         		min_servo_direction = min_direction_seekbar.getProgress();
-                Log.d("BT", String.valueOf(min_servo_direction));
+        		txt_view_min_direction.setText(String.valueOf(min_servo_direction));
         	}
 			public void onStartTrackingTouch(SeekBar seekBar) { }
 			public void onStopTrackingTouch(SeekBar seekBar) { }
@@ -121,7 +127,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         max_direction_seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
         	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         		max_servo_direction = max_direction_seekbar.getProgress();
-                Log.d("BT", String.valueOf(max_servo_direction));
+        		txt_view_max_direction.setText(String.valueOf(max_servo_direction));
         	}
 			public void onStartTrackingTouch(SeekBar seekBar) { }
 			public void onStopTrackingTouch(SeekBar seekBar) { }
@@ -152,6 +158,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 					    connection.connect();
 				    } catch (IOException e) {
     					e.printStackTrace();
+    					Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 	    			}
 		    	}
 			}
@@ -160,7 +167,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     
     private void setupButtons() {
     	ToggleButton trimmers = (ToggleButton) findViewById(R.id.trimmers);
-    	bt_connection = (ToggleButton) findViewById(R.id.connection);
+    	Button bt_connection = (Button) findViewById(R.id.connection);
     	
     	trimmers.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
     	    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -168,10 +175,10 @@ public class MainActivity extends Activity implements SensorEventListener {
     	    }
     	});
     	
-    	bt_connection.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-    	    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-    	        setupBluetooth();
-    	    }
+        bt_connection.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				setupBluetooth();
+			}
     	});
     }
     
@@ -185,14 +192,14 @@ public class MainActivity extends Activity implements SensorEventListener {
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
 	public void onSensorChanged(SensorEvent event) {
-		if (!ready)
+		if (!sensor_ready || !bt_ready)  // FIXME: I think that this is not needed, the listener will not be called without ready sensor lol
 			return;
 		
-		y = event.values[1] + 7; //7 is the minimal angle from the left 
-		z = event.values[2] + 3; //The range on the mobile is from -3 to 7, this will do the calculation easier :)
+		y = event.values[1] + 7; //7 is the minimal angle from the left
+		z = event.values[2] + 3; //The range on the mobile is from -3 to 12, this will do the calculation easier :)
 		
 		int range_speed = max_servo_speed - min_servo_speed;
-		int offset_speed = range_speed / 13; // 13 are the degrees of freedom in the speed (from -3 to 10)
+		int offset_speed = range_speed / 16; // 13 are the degrees of freedom in the speed (from -3 to 12)
 		
 		int range_direction = max_servo_direction - min_servo_direction;
 		int offset_direction = range_direction / 15; // 15 are the degrees of freedom in the direction
@@ -215,12 +222,14 @@ public class MainActivity extends Activity implements SensorEventListener {
 				//bt_connection.setChecked(true);
 				connection.getOutputStream().write((char) to_send[0]);
 				connection.getOutputStream().write((char) to_send[1]);
+				connection.getOutputStream().flush();
 				//connection.getOutputStream().write((char) '\n');
 			} catch (IOException e) {
 				//bt_connection.setChecked(false);
 				// FIXME: Deactivated because call this without any delay breaks the app
 				//setupBluetooth();
 				e.printStackTrace();
+				Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 			}
 		}
 		
